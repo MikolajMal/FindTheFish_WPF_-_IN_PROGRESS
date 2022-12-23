b13f7ec73cc53a4d5c229b858c7c18a796e682e5
Mikolaj <mikolaj.gamedev@gmail.com>
2022-12-23 19:37:56 +0100

    Add frame around current selected action

--- FindTheFish/MainWindow.xaml.cs
+++ FindTheFish/MainWindow.xaml.cs
@@ -21,25 +21,40 @@ namespace FindTheFish
     public partial class MainWindow : Window
     {
         ImageSource? currentImage;
+        Border borderOfCurrentAction = new Border();
 
         public MainWindow()
         {
             InitializeComponent();
+            borderOfCurrentAction.BorderThickness = new Thickness(2, 2, 2, 2);
+            ActionButtonsGrid.Children.Add(borderOfCurrentAction);
         }
 
         private void ButtonClicked_RosaStoi(object sender, RoutedEventArgs e)
         {
             currentImage = RosaStoi.Source;
+            
+            Grid.SetColumn(borderOfCurrentAction, Grid.GetColumn(RosaStoi));
+            Grid.SetRow(borderOfCurrentAction, Grid.GetRow(RosaStoi));
+            borderOfCurrentAction.BorderBrush = new SolidColorBrush(Colors.Black);
         }
 
         private void ButtonClicked_RosaLezy(object sender, RoutedEventArgs e)
         {
             currentImage = RosaLezy.Source;
+
+            Grid.SetColumn(borderOfCurrentAction, Grid.GetColumn(RosaLezy));
+            Grid.SetRow(borderOfCurrentAction, Grid.GetRow(RosaLezy));
+            borderOfCurrentAction.BorderBrush = new SolidColorBrush(Colors.Black);
         }
 
         private void ButtonClicked_RosaPatrzy(object sender, RoutedEventArgs e)
         {
             currentImage = RosaPatrzy.Source;
+
+            Grid.SetColumn(borderOfCurrentAction, Grid.GetColumn(RosaPatrzy));
+            Grid.SetRow(borderOfCurrentAction, Grid.GetRow(RosaPatrzy));
+            borderOfCurrentAction.BorderBrush = new SolidColorBrush(Colors.Black);
         }
 
         private void Field_Clicked(object sender, MouseButtonEventArgs e)
